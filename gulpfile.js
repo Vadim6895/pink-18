9830c8ef2ce89e5b7c4f4ce89ff2d333ef3017fe
Vadim6895 <54281107+Vadim6895@users.noreply.github.com>
2019-11-27 23:28:17 +0300

    базовая стилизация

--- gulpfile.js
+++ gulpfile.js
@@ -31,6 +31,7 @@ gulp.task("server", function () {
   });
 
   gulp.watch("source/less/**/*.less", gulp.series("css"));
+  gulp.watch("source/blocks/**/*.less", gulp.series("css"));
   gulp.watch("source/*.html").on("change", server.reload);
 });
 
