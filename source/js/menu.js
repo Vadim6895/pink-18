2adcf7a575d71eeeccabb5b20f15b9a0f617e55a
Vadim6895 <54281107+Vadim6895@users.noreply.github.com>
2020-01-24 23:45:17 +0300

    #5

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,5 +1,6 @@
 var mainnav = document.querySelector('.main-nav');
 var navtoggle = document.querySelector('.main-nav__toggle');
+var page_header = document.querySelector('.page-header');
 
 mainnav.classList.remove('main-nav--no-js');
 
@@ -7,8 +8,10 @@ navtoggle.addEventListener('click', function() {
   if (mainnav.classList.contains('main-nav--closed')) {
     mainnav.classList.remove('main-nav--closed');
     mainnav.classList.add('main-nav--opened');
+    page_header.classList.add('page-header--color');
   } else {
     mainnav.classList.add('main-nav--closed');
     mainnav.classList.remove('main-nav--opened');
+    page_header.classList.remove('page-header--color');
   }
 });
